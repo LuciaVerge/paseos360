e4e2e5240351b80e25ca8eb47db38d31c753624a
Lu <luciaverge12@hotmail.com>
2023-07-05 16:48:17 -0300

    agrego link al portfolio en "mas servicios"

---
+++ smooth.js
@@ -0,0 +1,21 @@
+// Capturamos el evento de scroll
+window.addEventListener('wheel', handleWheelScroll);
+
+// Función para el desplazamiento suave
+function handleWheelScroll(event) {
+  event.preventDefault(); // Evitamos el desplazamiento predeterminado
+
+  // Obtener la distancia de desplazamiento
+  const delta = Math.sign(event.deltaY);
+  const scrollDistance = 1300; // Distancia de desplazamiento
+
+  // Calcular la posición de desplazamiento
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const newPosition = scrollTop + (scrollDistance * delta);
+
+  // Realizar el desplazamiento suave
+  window.scrollTo({
+    top: newPosition,
+    behavior: 'smooth'
+  });
+}
