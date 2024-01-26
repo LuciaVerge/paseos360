c63baba1d56dcdef3b879b1d20b5e1333ed71114
Lu <luciaverge12@hotmail.com>
2024-01-26 12:38:45 -0300

    doy animacion al slider

--- menu.js
+++ menu.js
@@ -1,3 +1,5 @@
+//visibilidad del menu hamburguesa
+
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
@@ -8,4 +10,18 @@ abrir.addEventListener("click", () => {
 
 cerrar.addEventListener("click" , () => {
     nav.classList.remove("visible");
+})
+
+
+//animacion infinita del slider de logos
+
+document.addEventListener('DOMContentLoaded', function () {
+  const brands = document.querySelector('ul.brands');
+  const total_brands = brands.children.length;
+
+  document.documentElement.style.setProperty('--total-brand', total_brands);
+
+  for (let i = 0; i < total_brands; i++) {
+      brands.appendChild(brands.children[i].cloneNode(true));
+  }
 })
